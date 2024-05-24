9c4470ede0913aab0d517e3edd9cc88f02813c60
AkitaShohei <s2222001@stu.musashino-u.ac.jp>
2024-05-24 18:27:04 +0900

    initを追加

--- main.py
+++
@@ -1,9 +0,0 @@
-from clipboard_manager import ClipboardManager as pp
-
-# クリップボードの履歴から特定のキーワードを含むアイテムを検索
-keywords = ["Python", "Machine Learning"]
-for keyword in keywords:
-    history_items = pp.search_history(keyword)
-    print(f"{keyword}に関するクリップボードの履歴:")
-    for item in history_items:
-        print(item)
