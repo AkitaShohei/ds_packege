65660558427c26e6c65dc195766d82c7d76a9d8f
AkitaShohei <s2222001@stu.musashino-u.ac.jp>
2024-05-24 16:03:19 +0900

    pyperclip_plus

---
+++ setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='pyperclip_plus',
+    version='0.1',
+    packages=find_packages(),
+    description='An enhanced clipboard management tool',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/pyperclip_plus',  # プロジェクトのURLを設定
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
